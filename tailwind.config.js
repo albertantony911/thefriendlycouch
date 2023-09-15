fc288009456536e24db03a6db7a7f9a74667a2be
albertantony911 <66047048+albertantony911@users.noreply.github.com>
2023-09-15 15:42:37 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+
+    colors: {
+      'light-purple': '#1fb6ff',
+      'dark-purple': '#463174',
+    },
+
+
+    extend: {},
+  },
+  plugins: [],
+}
