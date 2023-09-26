a732672c5e1d100651ab9fb74fabf35d86fa6c9d
albertantony911 <66047048+albertantony911@users.noreply.github.com>
2023-09-26 21:42:14 +0530

    new breakpoints update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,20 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-
     colors: {
       'light-purple': '#D1BEF9',
       'dark-purple': '#463174',
       'mid-purple': '#593F92',
       'light': '#FFFFFF',
     },
-
     container: {
       center: true,
     },
-
-    
-
-
     extend: {
-
-      fontFamily:{
-        'poppins': ['Poppins']
-      }
-
+      screens: {
+        'lg': { 'min': '1280px', 'raw': '(min-device-width: 1280px) and (-webkit-min-device-pixel-ratio: 2)' }, // Use 'lg' for MacBook
+        'tablet': { 'min': '1024px', 'raw': '(min-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2)' }, // Use 'tablet' for iPad Pro
+      },
     },
   },
   plugins: [],
