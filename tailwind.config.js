24da3502c8a944264ddafbefa82fc8b9c9dcc15f
albertantony911 <66047048+albertantony911@users.noreply.github.com>
2023-09-27 20:45:11 +0530

    Hero section update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    colors: {
-      'light-purple': '#D1BEF9',
-      'dark-purple': '#463174',
-      'mid-purple': '#593F92',
-      'light': '#FFFFFF',
-    },
-    container: {
-      center: true,
-    },
     extend: {
-      screens: {
-        'lg': { 'min': '1280px', 'raw': '(min-device-width: 1280px) and (-webkit-min-device-pixel-ratio: 2)' }, // Use 'lg' for MacBook
-        'tablet': { 'min': '1024px', 'raw': '(min-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2)' }, // Use 'tablet' for iPad Pro
+      container: {
+        center: true,
+      },
+      
+     
+      colors: {
+        'light-purple': '#D1BEF9',
+        'dark-purple': '#463174',
+        'mid-purple': '#593F92',
+        'light': '#FFFFFF',
       },
     },
   },
   plugins: [],
-}
+};
