3f5567b21eb1f42009a1b8648058100cb1d7630f
albertantony911 <66047048+albertantony911@users.noreply.github.com>
2023-11-14 13:58:21 +0530

    reorganised files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-    // Languages transition start
+   var bouncingBall = anime({
+	targets: '.couch',
+	translateY: '0.3rem',
+	duration: 400,
+	loop: true,
+	direction: 'alternate',
+	easing: 'easeInCubic',
+	scaleY: {
+		value: 0.98,
+		duration: 250,
+		delay: 250
+    },
+    scaleX: {
+		value: 1.01,
+		duration: 250,
+		delay: 250
+    }
+    
+    
+});
+   
+
+
+// Languages transition start
 
 
 document.addEventListener('DOMContentLoaded', function () {
