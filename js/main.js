83151d4d5d229f1e445783ad242b64ab715b2d33
albertantony911 <66047048+albertantony911@users.noreply.github.com>
2023-11-20 12:04:04 +0530

    about page basics

--- js/main.js
+++ js/main.js
@@ -1,47 +1,4 @@
-   var bouncingBall = anime({
-	targets: '.couch',
-	translateY: '0.3rem',
-	duration: 400,
-	loop: true,
-	direction: 'alternate',
-	easing: 'easeInCubic',
-	scaleY: {
-		value: 0.98,
-		duration: 250,
-		delay: 250
-    },
-    scaleX: {
-		value: 1.01,
-		duration: 250,
-		delay: 250
-    }
-     
-   });
-
-
-   var bouncingButton = anime({
-	targets: '.bouncingbutton',
-	translateY: '0.7rem',
-	duration: 400,
-	loop: true,
-	direction: 'alternate',
-	easing: 'easeInCubic',
-	scaleY: {
-		value: 0.9,
-		duration: 250,
-		delay: 250
-    },
-    scaleX: {
-		value: 1.03,
-		duration: 250,
-		delay: 250
-    }
-     
-});
-
-
-
-
+   
 // Languages transition start
 
 
